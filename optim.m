7c17477ccc7be6106f9b6bbc458b5b8abfc6cc97
nnogueirasgarc <150817326+nnogueirasgarc@users.noreply.github.com>
2026-03-28 16:35:01 +0100

    .

--- optim.m
+++ optim.m
@@ -1,5 +1,5 @@
 function range = optim(x, TestAC_data)
-    state_converged = run_mda(x);
+    state_converged = mda(x);
     
     state_converged = breguet(state_converged, TestAC_data.dv, TestAC_data.ac, TestAC_data.atm, TestAC_data.mis, TestAC_data.print_flag);
 
