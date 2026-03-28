6d1c8d5d99d7bd61f37457b59b5400d56da0102b
nnogueirasgarc <150817326+nnogueirasgarc@users.noreply.github.com>
2026-03-28 17:08:31 +0100

    .

--- constraints.m
+++ constraints.m
@@ -2,9 +2,9 @@ function [g, h] = constraints(x, x_consts, TestAC_data, options_mda)
 
     state_converged = mda(x, x_consts, TestAC_data, options_mda);
     g = zeros(3, 1);
-    g(1) = clearance(state_converged, TestAC_data.eng, TestAC_data.con);
-    g(2) = noise(state_converged, TestAC_data.dv, TestAC_data.atm, TestAC_data.thermo, TestAC_data.con);
-    g(3) = tit(state_converged, TestAC_data.con);
+    g(1) = clearance(state_converged, TestAC_data.eng);
+    g(2) = noise(state_converged, x, x_consts, TestAC_data.atm, TestAC_data.thermo);
+    g(3) = tit(state_converged, TestAC_data.eng);
     
     h = []; 
 end
