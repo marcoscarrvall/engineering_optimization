8c82f6f0d57dd448247042f1d1d730e80d0a1673
marcoscarrvall <150817672+marcoscarrvall@users.noreply.github.com>
2026-03-28 16:33:30 +0100

    mda

--- disciplines/mda.m
+++ disciplines/mda.m
@@ -1,41 +1,40 @@
-clear; clc;
+function state = mda(x, x_consts, data, options)
+tol = options.tol;
+max_iter = options.max_iter;
+verbose = options.verbose;
 
-data = TestAC_data;
-x.BPR = 14.0;
-x.V   = sqrt(data.atm.gamma * data.atm.R * data.atm.T_cruise) * 0.78;
-
-x_consts.PR_fan = 1.6;
-x_consts.PR_LPC = 2.6;
-x_consts.PR_HPC = 6.1;
-
+%% ---- 1. Unpack data ----------------------------------------------------
 atm         = data.atm;
 thermo_data = data.thermo;
 eng         = data.eng;
 wate_consts = data.wate;
 ac          = data.ac;
 
-%% ---- Initialisation ----------------------------------------------------
-state        = data.state;
-ac.S_ref  = (ac.MTOW - ac.W_fuel/2) * atm.g / ...
-            (0.5 * ac.CL_cruise * atm.rho_cruise * x.V^2);
-state.D_fan  = eng.D_fan_ref;
-state.L_eng  = eng.L_eng_ref;
+% Override S_ref to be consistent with cruise lift balance at mid-cruise weight
+ac.S_ref = (ac.MTOW - ac.W_fuel/2) * atm.g / ...
+           (0.5 * ac.CL_cruise * atm.rho_cruise * x.V^2);
 
-% Approximate initial thrust from MTOW and assumed L/D = 17.5
+%% ---- 2. Initialisation -------------------------------------------------
+state           = data.state;
+state.MTOW      = ac.MTOW;
+state.D_fan     = eng.D_fan_ref;
+state.L_eng     = eng.L_eng_ref;
+state.S         = ac.S_ref;
+
+% Initial drag from MTOW and assumed L/D
 state.D_cruise  = ac.MTOW * atm.g / 17.5;
 
-% Approximate initial mdot from thermo at that thrust
-state.MTOW      = ac.MTOW;
-state           = thermo(state, x, x_consts, atm, thermo_data, ac);
+% Initial mdot from thermo at that thrust
+state = thermo(state, x, x_consts, atm, thermo_data, ac);
 
-%% ---- MDA loop ----------------------------------------------------------
-tol        = 1e-4;   
-max_iter   = 50;
-converged  = false;
+%% ---- 3. MDA loop -------------------------------------------------------
+if verbose
+    fprintf('%-6s  %-12s  %-12s  %-12s  %-10s  %-10s\n', ...
+        'Iter', 'MTOW [kg]', 'D_cruise [N]', 'mdot [kg/s]', 'D_fan [m]', 'rel_dMTOW');
+    fprintf('%s\n', repmat('-', 1, 70));
+end
 
-fprintf('%-6s  %-12s  %-12s  %-12s  %-10s  %-10s\n', ...
-    'Iter', 'MTOW [kg]', 'D_cruise [N]', 'mdot [kg/s]', 'D_fan [m]', 'rel_dMTOW');
-fprintf('%s\n', repmat('-', 1, 70));
+converged = false;
 
 for iter = 1:max_iter
     MTOW_prev = state.MTOW;
@@ -46,8 +45,10 @@ for iter = 1:max_iter
 
     rel_change = abs(state.MTOW - MTOW_prev) / MTOW_prev;
 
-    fprintf('%-6d  %-12.1f  %-12.1f  %-12.2f  %-10.4f  %-10.2e\n', ...
-        iter, state.MTOW, state.D_cruise, state.mdot, state.D_fan, rel_change);
+    if verbose
+        fprintf('%-6d  %-12.1f  %-12.1f  %-12.2f  %-10.4f  %-10.2e\n', ...
+            iter, state.MTOW, state.D_cruise, state.mdot, state.D_fan, rel_change);
+    end
 
     if rel_change < tol
         converged = true;
@@ -55,10 +56,19 @@ for iter = 1:max_iter
     end
 end
 
-%% ---- Result ------------------------------------------------------------
-fprintf('%s\n', repmat('-', 1, 70));
-if converged
-    fprintf('Converged in %d iterations (rel_dMTOW < %.0e)\n\n', iter, tol);
-else
-    fprintf('WARNING: did not converge in %d iterations\n\n', max_iter);
+%% ---- 4. Convergence report ---------------------------------------------
+if verbose
+    fprintf('%s\n', repmat('-', 1, 70));
+    if converged
+        fprintf('Converged in %d iterations (rel_dMTOW < %.0e)\n\n', iter, tol);
+    else
+        fprintf('WARNING: did not converge in %d iterations\n\n', max_iter);
+    end
+end
+
+if ~converged
+    warning('mda:noConvergence', ...
+        'MDA did not converge in %d iterations. Results may be unreliable.', max_iter);
+end
+
 end
